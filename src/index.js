705bf701c6951173e44701a9e9fb5fa25f4ef18c
Cynthia Trujillo <developercynthia@gmail.com>
2020-09-07 16:46:07 -0500

    Interactive Component Rendered X

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+ 
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
 }
- 
+
 class Board extends React.Component {
   renderSquare(i) {
     return <Square value={i}/>;
