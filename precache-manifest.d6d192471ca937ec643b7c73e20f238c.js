bd5711bf0a0d33bb525bf099c9b8c93b9db40298
Cynthia Trujillo <developercynthia@gmail.com>
2020-09-17 13:35:47 -0500

    Updates

---
+++ precache-manifest.d6d192471ca937ec643b7c73e20f238c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0083ff80b952010854f24fb88d6af0e8",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d73d48ffbf15672cb11",
+    "url": "/tic-tac-toe/static/js/main.0de09963.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
